cf4be9039351b539d3e42dabbb0fbaec3c6b16b2
beno1234 <benolopesdias@gmail.com>
2025-01-02 11:47:45 -0300

    update carousel with dinamic images

--- src/Pages/Vida/Section4/SectionNovelas.tsx
+++ src/Pages/Vida/Section4/SectionNovelas.tsx
@@ -1,69 +1,82 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import roqueImage from "../../../assets/roque-santeiro.png";
 import pantanalImage from "../../../assets/pantanal.png";
 import maysaImage from "../../../assets/maysa.png";
 import decoration2 from "../../../assets/decoration2.png";
+import vendedorDeSonhos from "../../../assets/vendedor-de-sonhos.jpg";
+import oTempoeoVento from "../../../assets/O-Tempo-e-o-Vento-5.jpeg";
+import seteMulheres from "../../../assets/A-Casas-das-7-Mulheres.jpg";
+import chiquinho from "../../../assets/chiquinho.jpg";
+import terraNostra from "../../../assets/terra-nostra.jpg";
+
+import AventalRosa from "../../../assets/O-avental-rosa-scaled.jpg";
+
+import olga from "../../../assets/Olga-7.jpg";
+import { useState } from "react";
+
+type ContentImage = {
+  image: string[];
+};
 
 const SectionNovelas = () => {
+  const [activeTab, setActiveTab] = useState<keyof typeof content>("Filmes");
+
+  const content: Record<string, ContentImage> = {
+    Novelas: {
+      image: [roqueImage, pantanalImage, terraNostra],
+    },
+    Filmes: {
+      image: [vendedorDeSonhos, oTempoeoVento, olga], // Adicione imagens específicas para Filmes
+    },
+    "Mini-Series e Series": {
+      image: [seteMulheres, chiquinho, maysaImage], // Adicione imagens específicas para Series
+    },
+    Lançamentos: {
+      image: [AventalRosa], // Adicione imagens específicas para Series
+    },
+  };
+
+  const handleTabClick = (tab: keyof typeof content) => {
+    setActiveTab(tab);
+  };
+
   return (
     <section className="relative bg-[#1C2415] text-white py-8 sm:py-16">
       <div className="relative max-w-screen-xl mx-auto px-4 sm:px-8">
         {/* Navegação */}
         <nav className="flex justify-center overflow-x-auto">
           <ul className="flex flex-wrap justify-center gap-4 sm:gap-8 text-base sm:text-lg font-normal font-glonimove">
-            <li>
-              <a className="opacity-40 cursor-pointer hover:opacity-100 transition-opacity">
-                Mini Séries
+            {Object.keys(content).map((tab) => (
+              <a
+                key={tab}
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleTabClick(tab as keyof typeof content);
+                }}
+                className={`hover:text-white ${
+                  activeTab === tab
+                    ? "text-white font-bold opacity-100"
+                    : "opacity-50"
+                }`}
+              >
+                {tab}
               </a>
-            </li>
-            <li>
-              <a className="opacity-30 cursor-pointer hover:opacity-100 transition-opacity">
-                Séries
-              </a>
-            </li>
-            <li>
-              <a className="font-bold cursor-pointer">Novelas</a>
-            </li>
-            <li>
-              <a className="opacity-20 cursor-pointer hover:opacity-100 transition-opacity">
-                Filmes
-              </a>
-            </li>
-            <li>
-              <a className="opacity-30 cursor-pointer hover:opacity-100 transition-opacity">
-                Outros
-              </a>
-            </li>
+            ))}
           </ul>
         </nav>
 
         {/* Grid Responsivo */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 mt-12 sm:mt-16">
-          {/* Card 1 */}
-          <div className="flex flex-col items-center text-center">
-            <img
-              src={roqueImage}
-              alt="Roque Santeiro"
-              className="w-full max-w-sm h-auto object-cover rounded-md mb-4"
-            />
-          </div>
-
-          {/* Card 2 */}
-          <div className="flex flex-col items-center text-center">
-            <img
-              src={pantanalImage}
-              alt="Pantanal"
-              className="w-full max-w-sm h-auto object-cover rounded-md mb-4"
-            />
-          </div>
-
-          {/* Card 3 */}
-          <div className="flex flex-col items-center text-center">
-            <img
-              src={maysaImage}
-              alt="Maysa: Quando Fala o Coração"
-              className="w-full max-w-sm h-auto object-cover rounded-md mb-4"
-            />
-          </div>
+          {content[activeTab].image.map((img, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <img
+                src={img}
+                alt={`Imagem ${index + 1}`}
+                className="w-full h-[200px] sm:h-[500px] object-cover rounded-md mb-4"
+              />
+            </div>
+          ))}
         </div>
       </div>
 
